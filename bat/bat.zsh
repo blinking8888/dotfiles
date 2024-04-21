4d9342294f10f0faf57968de31de05c2a84a32c1
Nestor Aguilar <2113804+blinking8888@users.noreply.github.com>
2024-04-21 16:58:36 +0800

    fix: Don't fail if a program does not exist

--- bat/bat.zsh
+++ bat/bat.zsh
@@ -1,10 +1,15 @@
 #!/bin/zsh
-export MANPAGER="sh -c 'col -bx | bat -l man -p'"
-alias bathelp='bat --plain --language=help'
 
-help() {
-    "$@" --help 2>&1 | bathelp
-}
+if [[ -x $(which bat) ]] ; then
+    echo "bat is installed, configuring bat.."
+    export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+    alias bathelp='bat --plain --language=help'
 
-alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
-alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
+    help() {
+        "$@" --help 2>&1 | bathelp
+    }
+
+    alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
+    alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
+fi
+
--- eza/eza.zsh
+++ eza/eza.zsh
@@ -1,2 +1,8 @@
-alias ls='eza --icons=always --color=always --long --git --no-filesize --no-time --no-user --no-permissions'
-alias ll='eza --icons=always --tree --color=always --long --level 3'
+#!/bin/sh
+
+if [[ -x $(which eza) ]] ; then
+    echo "eza found, aliasing it as ls"
+    alias ls='eza --icons=always --color=always --long --git --no-filesize --no-time --no-user --no-permissions'
+    alias ll='eza --icons=always --tree --color=always --long --level 3'
+fi
+
