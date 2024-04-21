0ae22e3c3b04a2e27fccde252feb49c78ef419d7
Nestor Aguilar <2113804+blinking8888@users.noreply.github.com>
2024-04-21 17:22:04 +0800

    fix: Only echo something if skipping configuration
    Remove the message that may be annoying as you'd always see this on
    every new shell.

--- bat/bat.zsh
+++ bat/bat.zsh
@@ -1,7 +1,6 @@
 #!/bin/zsh
 
 if [[ -x $(which bat) ]] ; then
-    echo "bat is installed, configuring bat.."
     export MANPAGER="sh -c 'col -bx | bat -l man -p'"
     alias bathelp='bat --plain --language=help'
 
@@ -11,5 +10,7 @@ if [[ -x $(which bat) ]] ; then
 
     alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
     alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
+else
+    echo "bat is not found, skipping configuration"
 fi
 
--- eza/eza.zsh
+++ eza/eza.zsh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 if [[ -x $(which eza) ]] ; then
-    echo "eza found, aliasing it as ls"
     alias ls='eza --icons=always --color=always --long --git --no-filesize --no-time --no-user --no-permissions'
     alias ll='eza --icons=always --tree --color=always --long --level 3'
+else
+    echo "eza not found, skipping configuration"
 fi
 
