3010ac6441595d354c2f00b20d1cb1d89b0bb838
Nestor Aguilar <2113804+blinking8888@users.noreply.github.com>
2024-04-22 09:24:29 +0800

    nvim: Update indent-blankline configuration

--- nvim/lua/s2r/plugins/indent-blankline.lua
+++ nvim/lua/s2r/plugins/indent-blankline.lua
@@ -1,7 +1,11 @@
 return {
   {
     {
-      "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {}
+      "lukas-reineke/indent-blankline.nvim",
+      opts = {
+        char = '┊',
+        show_trailing_blankline_indent = false,
+      }
     }
   },
 }
