22007ff4c40e7f494e7cab024350a2501076d654
Nestor Aguilar <2113804+blinking8888@users.noreply.github.com>
2024-05-19 10:50:45 +0800

    eza: Add 'la' command to show user:group

--- eza/eza.zsh
+++ eza/eza.zsh
@@ -2,6 +2,7 @@
 
 if [[ -x $(which eza) ]] ; then
     alias ls='eza --icons=always --color=always --long --git --no-filesize --no-time --no-user --no-permissions'
+    alias la='eza --icons=always --color=always --long --git -lag'
     alias ll='eza --icons=always --tree --color=always --long --level 3'
 else
     echo "eza not found, skipping configuration"
